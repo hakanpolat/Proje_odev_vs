6e5a894f1a71af34f85ee6f0c32560761b55c2c7
hakanpolat <polat94hakan@gmail.com>
2017-04-18 01:41:35 +0300

    compansation bitmiş haldedir. kablo tipi seçilecek. loadprofile.xlsx'e fazladan 1 tane leading pf li load eklenmiştir. çalışıyor denendi

--- ee374proje.m
+++ ee374proje.m
@@ -4,7 +4,8 @@ clc;
 [status,sheets] = xlsfinfo('load_profile.xlsx');
 numOfSheets = numel(sheets);
 % num of sheets ile kaç tane load var onlar? ö?reniyorum
-
+zz=zeros(24,1);
+compansation=zeros(1,numOfSheets);
 
 num=xlsread('load_profile.xlsx',1);
     %ilk loadi num a yazd?r?yorum
@@ -32,10 +33,12 @@ end
 for bb=1:numOfSheets
     for cc= 1:24
         
-        if (q(cc,bb)>0) &&((p(cc,bb)/sqrt(p(cc,bb)^2+q(cc,bb)^2))<0.95)
+        if (q(cc,bb)>0) &&((p(cc,bb)/sqrt(p(cc,bb)^2+q(cc,bb)^2))<0.95);
             comp(cc,bb)=q(cc,bb)-(p(cc,bb)/0.95)*sqrt(1-0.95^2);
-        elseif (q(cc,bb)<0) &&((p(cc,bb)/sqrt(p(cc,bb)^2+q(cc,bb)^2))<0.98)
-            comp(cc,bb)=q(cc,bb)-(p(cc,bb)/0.95)*sqrt(1-0.98^2);
+            %lagging
+        elseif (q(cc,bb)<0)&&((p(cc,bb)/sqrt(p(cc,bb)^2+q(cc,bb)^2))<0.98);
+            comp(cc,bb)=q(cc,bb)+(p(cc,bb)/0.98)*sqrt(1-0.98^2);
+            %lead
         else
             comp(cc,bb)=0;        
     end   
@@ -43,6 +46,44 @@ for bb=1:numOfSheets
        
 end
     % comp fonksiyonu kompanzasyon degerlerini iceriyor.
+    comp1=sort(comp);
+    %comp1 büyükten kucuge s?ral?yor
     
-
+    for n=1:numOfSheets
+        if abs(comp1(24,n))<abs(comp1(1,n));
+            compansation(1,n)=comp1(1,n);
+        else abs(comp1(24,n))>abs(comp1(1,n));
+            compansation(1,n)=comp1(24,n);
+        end 
+    end
+    
+    %burdaki for leading pf leri de i?in içine katmak için bulunuyor.
+            
+  %companzasyon matrixi tüm loadlar için gereken kompanzasyonu veriyor.
+  %bu andan sonra tüm loadlar? kompanze edilmi? haline çevirebiliriz.
+ 
+    for bb=1:numOfSheets
+    for cc= 1:24
+        compansatedload(cc,bb*2-1)=p(cc,bb);
+          if (q(cc,bb)>0) &&((p(cc,bb)/sqrt(p(cc,bb)^2+q(cc,bb)^2))<0.95);
+            compansatedload(cc,bb*2)=(p(cc,bb)/0.95)*sqrt(1-0.95^2);
+        elseif (q(cc,bb)<0)&&((p(cc,bb)/sqrt(p(cc,bb)^2+q(cc,bb)^2))<0.98);
+            compansatedload(cc,bb*2)=-(p(cc,bb)/0.98)*sqrt(1-0.98^2);
+        else
+            comp(cc,bb)=0;   
+             
+    end 
+       
+    end
+    end
+    %compansatedload bize companse edilmis loadlar?n p ve qsuna sahip olan
+    %loadlar? veriyor.
+    
+    
+    
+    
+    %%Buradan sonra kablo tipi seçme i?ine giricez.
+    
+    cableareas=[240,180,120,70];
+    
     
